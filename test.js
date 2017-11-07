b3f458fbf08b6dbe8d64f686e35986d0bc0b2035
prodionov <pavelrodionov@hotmail.com>
2017-11-07 13:04:35 +0200

    the first test

--- test.js
+++ test.js
@@ -1,7 +1,17 @@
 var test = require('tape');
 var logic = require('./logic');
 
+var toDo = logic.addTodo;
+console.log(logic.addTodo);
+
+
+
 test('Example test', function(t) {
-  t.pass();
+  var actual = toDo([], 'make coffee');
+  var expected = [{'id' : 1,
+              'description' : 'make coffee',
+              'done' : false}];
+  //t.equal(actual, expected);
+  t.equal(actual, expected, 'The task should look like:' + String(expected.description));
   t.end();
 });
